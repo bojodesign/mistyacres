b122f16b264b6da85f22834786773782f7638d67
TankedThomas <TankedThomas@users.noreply.github.com>
2023-03-04 17:39:05 +1300

    Add instructions for quotes

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -6,15 +6,15 @@ import ProductCard from './ProductCard';
 
 function Store() {
 	const [filter, setFilter] = useState('aval');
-  
+
 	const filteredProducts = productsArray.filter((product) =>
-	  product.category.includes(filter)
+		product.category.includes(filter)
 	);
 
 	// Render Store
 	return (
 		<>
-		<section className="page-section bg-purple text-white portfolio" id="catalogue">
+			<section className="page-section bg-purple text-white portfolio" id="catalogue">
 				<Container>
 					<h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Catalogue</h2>
 					<div className="divider-custom">
@@ -24,6 +24,13 @@ function Store() {
 						</div>
 						<div className="divider-custom-line"></div>
 					</div>
+					<Row className="text-secondary mb-4">
+						<Col className="text-center">
+							<div className="lead">Please select the plants you would like to order and submit the contact form.</div>
+							<div className="lead">We will get back to you shortly with a final quote and delivery cost.</div>
+							<div className="lead">Minimum order of 50 plants of any kind.</div>
+						</Col>
+					</Row>
 					<div className="d-flex justify-content-center">
 						<div className="tabs">
 							<Button className="mx-2 btn btn-primary bg-red fw-bold" id="filt" active={filter === 'aval'} onClick={() => setFilter("aval")}>Available</Button>
@@ -31,16 +38,16 @@ function Store() {
 						</div>
 					</div>
 					<Row xs={1} md={2} className="g-1 card-background">
-					{filteredProducts.map((product) => (
+						{filteredProducts.map((product) => (
 							<Col align="center" key={`product.${product.id}`}>
-							<ProductCard product={product}/>
-						</Col>
+								<ProductCard product={product} />
+							</Col>
 						))}
 					</Row>
 				</Container>
-		</section>
+			</section>
 		</>
 	);
 }
-	
+
 export default Store;
