14c3bb2f583c748f1126a14b78e019d846a7c03d
TankedThomas <TankedThomas@users.noreply.github.com>
2023-03-04 17:51:19 +1300

    Require 50 items minimum in quote

--- src/components/QuoteSummary.jsx
+++ src/components/QuoteSummary.jsx
@@ -16,32 +16,33 @@ function QuoteSummary() {
 
 	return (
 		<div className="form-floating mb-3 quote-summary">
-			<div className="btn-light quote-label" onClick={() => setOpen(!open)} aria-controls="quote" aria-expanded={open}>Quote Summary: {productsCount} items</div>
-			<Collapse in={open}>
-				<div className="px-3" htmlFor="quote" name="quote" id="quote">
-					{productsCount > 0 ? (
-						<>
-							<Row className="justify-content-start align-items-top flex-nowrap">
-								<Col>Item</Col>
-								<Col>Quantity</Col>
-								<Col>Subtotal</Col>
-								<Col>Remove</Col>
-							</Row>
-							<hr className="mt-0 border-secondary" />
-							{cart.items.map((cartItem) => (
-								<CartProduct key={cartItem.id} {...cartItem} />
-							))}
-
-							<h5 className="pb-3">
-								{' '}
-								<span>Total:&nbsp;{totalCost}</span>
-							</h5>
-						</>
-					) : (
-						<div></div>
-					)}
+			{productsCount >= 50 ? (
+				<div>
+					<div className="btn-light quote-label" onClick={() => setOpen(!open)} aria-controls="quote" aria-expanded={open}>Quote Summary: {productsCount} items</div>
+					<Collapse in={open}>
+						<div className="px-3" htmlFor="quote" name="quote" id="quote">
+							<>
+								<Row className="justify-content-start align-items-top flex-nowrap">
+									<Col>Item</Col>
+									<Col>Quantity</Col>
+									<Col>Subtotal</Col>
+									<Col>Remove</Col>
+								</Row>
+								<hr className="mt-0 border-secondary" />
+								{cart.items.map((cartItem) => (
+									<CartProduct key={cartItem.id} {...cartItem} />
+								))}
+								<h5 className="pb-3">
+									{' '}
+									<span>Total:&nbsp;{totalCost}</span>
+								</h5>
+							</>
+						</div>
+					</Collapse>
 				</div>
-			</Collapse>
+			) : (
+				<div></div>
+			)}
 		</div>
 	);
 }
