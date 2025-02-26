2bce73e094e0c2c3eb046fee73e68361f5b3fea0
Thomas <TankedThomas@users.noreply.github.com>
2025-02-26 23:53:13 +1300

    Add trade list date via metadata extraction

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vite'
+import fileMetadataPlugin from './plugins/vite-plugin-metadata';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: "**/*.svg?react" })],
+  plugins: [react(),
+  svgr({ include: "**/*.svg?react" }),
+  fileMetadataPlugin({
+    targetFiles: [
+      'public/assets/tradelist.csv'
+    ],
+    metadataFile: 'src/fileMetadata.js'
+  })
+  ],
   usePolling: true,
 });
